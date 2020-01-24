70c1193f758bf9ddcedf243c27e6a244ea91fe43
Swanav <swanav@outlook.com>
2020-01-24 22:28:27 +0530

    Build files

---
+++ docs/precache-manifest.2af2af84bf09dace95c8c16ade2e42c8.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "08aa05fe06c49d4112acaa1bfe95db6a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6b2ea62f6e834fff2433",
+    "url": "/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "d0f3fab0f7fda6dae2dc",
+    "url": "/static/js/2.30f9b7e1.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/static/js/2.30f9b7e1.chunk.js.LICENSE"
+  },
+  {
+    "revision": "6b2ea62f6e834fff2433",
+    "url": "/static/js/main.aad8b8fc.chunk.js"
+  },
+  {
+    "revision": "54467a4a02d0b8bafbea",
+    "url": "/static/js/runtime-main.8829c197.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
